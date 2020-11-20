3e51dbf353938920e3d4cad00b917fd653cba95c
hockeyqqy <qqu@slb.com>
2020-11-20 16:18:50 +0800

    refactor: naming: add spacePattern

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -6,47 +6,48 @@ namespace WordFrequency
 {
     public class WordFrequencyGame
     {
+        private string spacePattern;
+
         public string GetResult(string inputStr)
         {
-            if (Regex.Split(inputStr, @"\s+").Length == 1)
+            spacePattern = @"\s+";
+            if (Regex.Split(inputStr, spacePattern).Length == 1)
             {
                 return inputStr + " 1";
             }
-            else
+
+            string[] splitStrings = Regex.Split(inputStr, spacePattern);
+
+            List<Input> inputList = new List<Input>();
+            foreach (var word in splitStrings)
             {
-                string[] splitStrings = Regex.Split(inputStr, @"\s+");
-
-                List<Input> inputList = new List<Input>();
-                foreach (var word in splitStrings)
-                {
-                    Input input = new Input(word, 1);
-                    inputList.Add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Dictionary<string, List<Input>> map = GetListMap(inputList);
-
-                List<Input> list = new List<Input>();
-                foreach (var entry in map)
-                {
-                    Input input = new Input(entry.Key, entry.Value.Count);
-                    list.Add(input);
-                }
-
-                inputList = list;
-
-                inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
-
-                List<string> strList = new List<string>();
-
-                foreach (Input input in inputList)
-                {
-                    string str = input.Value + " " + input.WordCount;
-                    strList.Add(str);
-                }
-
-                return string.Join("\n", strList.ToArray());
+                Input input = new Input(word, 1);
+                inputList.Add(input);
             }
+
+            //get the map for the next step of sizing the same word
+            Dictionary<string, List<Input>> map = GetListMap(inputList);
+
+            List<Input> list = new List<Input>();
+            foreach (var entry in map)
+            {
+                Input input = new Input(entry.Key, entry.Value.Count);
+                list.Add(input);
+            }
+
+            inputList = list;
+
+            inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
+
+            List<string> strList = new List<string>();
+
+            foreach (Input input in inputList)
+            {
+                string str = input.Value + " " + input.WordCount;
+                strList.Add(str);
+            }
+
+            return string.Join("\n", strList.ToArray());
         }
 
         private Dictionary<string, List<Input>> GetListMap(List<Input> inputList)
